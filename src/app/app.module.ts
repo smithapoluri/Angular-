d1c117e007e2c5c2d0fb02c1ff2cdd5324cbd012
Smitha Poluri <Smitha.poluri@trader.ca>
2018-03-27 09:40:29 -0700

    ang first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { ServerComponent } from './server/server.Component';
+import { ServersComponent } from './servers/servers.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SuccessAlertComponent } from './successalert/successalert.component';
+import { WarningAlertComponent } from './warningAlert/warningAlert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    NavbarComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent  
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/warningAlert/warningAlert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-warning',
+    templateUrl: './warningAlert.component.html'
+})
+export class WarningAlertComponent{
+
+}
