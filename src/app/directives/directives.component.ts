46c07e1a5906a2db30844c0ee7d41115d0891f08
Smitha Poluri <Smitha.poluri@trader.ca>
2018-03-29 16:53:50 -0700

    check

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -3,13 +3,51 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-directives',
   templateUrl: './directives.component.html',
-  styleUrls: ['./directives.component.css']
+  styles:[`
+  .count{
+    background: green;
+    color: white;
+  }
+`]
 })
 export class DirectivesComponent implements OnInit {
+showContent = false;
+events : Array<string> = [];
 
-  constructor() { }
+  constructor() { 
+    this.showContent = false;
+  }
 
   ngOnInit() {
   }
+public onPress(): void{
+  if(this.showContent)
+  {
+    this.showContent = false;
+  }else{
+    this.showContent = true;
+  }
+  this.events.push(event.timeStamp.toString());
+  this.getColor();
+ }
 
+ public ShowContent():string{
+   return this.showContent? 'block': 'none';
+ }
+
+ public getColor():string {
+   var length = this.events.length;
+  if(length!=0 && length>4)
+  {
+    return 'green';
+  }
+  else{
+    return 'white';
+  }
+ }
+
+ public EventsArray(): Array<string>{
+    return this.events;
+ }
 }
+ 
